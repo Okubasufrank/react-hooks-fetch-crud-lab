b3c51976e991c5b067ca569401b1ac5b95bc58c1
Okubasufrank <franklineokubasu51@gmail.com>
2022-07-22 05:40:47 +0300

    Done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
+import QuestionItem from "./QuestionItem.js";
+
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+ 
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(res => {
+      setQuestions(res);
+    
+    });
+  }, [])
+
+
+
+
+  function handleDeleteQuestions(id){
+  
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method : "DELETE"
+    })
+    .then(res => res.json())
+
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then(res => {
+      setQuestions(res);
+   
+    });
+  }
+
+  const questionItem = questions.map(quest => (
+    <QuestionItem question = { quest } onDelete = { handleDeleteQuestions }/>
+  ))
+  console.log(questions)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionItem}
+      </ul>
     </section>
   );
 }
